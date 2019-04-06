34705204da7de32426deda4ca8ccb00b132a7f7f
Dominic Okapal <djo@pdx.edu>
2019-04-06 13:15:27 -0700

    Added Audiogram part to summary

--- src/app/audiologist-summary/audiologist-summary.component.ts
+++ src/app/audiologist-summary/audiologist-summary.component.ts
@@ -12,13 +12,11 @@ import { TsScreenerAnswerStrings, ThsAnswerStrings } from '../common/custom-reso
 })
 export class AudiologistSummaryComponent implements OnInit {
   public patientID;
-  public ts;
-  public thsA;
-  public thsB;
-  public thsC;
-  public thsCtxt;
-  public thsCex;
-  public tfi;
+  public ts: string;
+  public thsA: number; public thsB: number;
+  public thsC: number; public thsCtxt: string; public thsCex: string;
+  public overallTFI: number; public intrusive: number; public sense: number; public cognitive: number;
+  public sleep: number; public auditory: number; public relaxation: number; public quality: number; public emotional: number;
 
   /**
    *
@@ -39,26 +37,26 @@ export class AudiologistSummaryComponent implements OnInit {
   ngOnInit() {
   }
 
-  public setTS(){
+  public setTS() {
     let answers = this.tsDataService.dataRecord;
     let tsAnswers = new TsScreenerAnswerStrings();
     let index = 0;
-    if(answers[index++].choice === tsAnswers.NO) {
+    if (answers[index++].choice === tsAnswers.NO) {
       this.ts = "No Tinnitus";
       return;
     }
 
     this.ts = (answers[index++].choice === tsAnswers.NO) ? "Acute" : "Chronic";
-    if(answers[index++].choice !== tsAnswers.SOMETIMES_OCCASIONALLY) {
+    if (answers[index++].choice !== tsAnswers.SOMETIMES_OCCASIONALLY) {
       this.ts += " Constant Tinnitus";
       return;
     }
-    if(answers[index++].choice === tsAnswers.YES_ALWAYS) {
+    if (answers[index++].choice === tsAnswers.YES_ALWAYS) {
       this.ts += " Temporary Tinnitus";
       return;
     }
-    if(answers[3].choice === tsAnswers.YES_SOMETIMES){
-      if(answers[index++].choice === tsAnswers.NO) {
+    if (answers[3].choice === tsAnswers.YES_SOMETIMES) {
+      if (answers[index++].choice === tsAnswers.NO) {
         this.ts += " Temporary Tinnitus";
         return;
       }
@@ -66,7 +64,7 @@ export class AudiologistSummaryComponent implements OnInit {
     this.ts += (answers[index].choice === tsAnswers.DAILY_OR_WEEKLY_BASIS) ? " Intermittent Tinnitus" : " Occasional Tinnitus";
   }
 
-  public setTHS(){
+  public setTHS() {
     let answers = this.thsDataService.dataRecord;
     this.thsA = this.sumTHS(answers, 0, 4);
     this.thsB = this.sumTHS(answers, 4, 4);
@@ -78,7 +76,7 @@ export class AudiologistSummaryComponent implements OnInit {
   private sumTHS(array, start, length): number {
     let sum: number = 0;
     length += start;
-    for(let i = start; i < length; i++) {
+    for (let i = start; i < length; i++) {
       sum += this.getTHSvalue(array, i);
     }
     return sum;
@@ -86,7 +84,7 @@ export class AudiologistSummaryComponent implements OnInit {
   private getTHSvalue(array, i): number {
     let thsAnswers = new ThsAnswerStrings();
     console.log(i + ": " + array[i].choice);
-    switch(array[i].choice) {
+    switch (array[i].choice) {
       case thsAnswers.NO: return 0;
       case thsAnswers.SMALL_YES: return 1;
       case thsAnswers.MODERATE_YES: return 2;
@@ -96,6 +94,34 @@ export class AudiologistSummaryComponent implements OnInit {
     }
   }
 
-  public setTFI(){}
+  public setTFI() {
+    let answers = this.tfiDataService.dataRecord;
+    // Calculate overall TFI score
+    this.overallTFI = 0;
+    for(let i = 0; i < answers.length; i++) {
+      this.overallTFI += answers[i].choice;
+    }
+    this.overallTFI /= 25;
+    this.overallTFI *= 10;
+
+    // Calculate subscores
+    this.intrusive = this.calcTFIsub(answers, 0, 3);
+    this.sense = this.calcTFIsub(answers, 3, 3);
+    this.cognitive = this.calcTFIsub(answers, 6, 3);
+    this.sleep = this.calcTFIsub(answers, 9, 3);
+    this.auditory = this.calcTFIsub(answers, 12, 3);
+    this.relaxation = this.calcTFIsub(answers, 15, 3);
+    this.quality = this.calcTFIsub(answers, 18, 4);
+    this.emotional = this.calcTFIsub(answers, 22, 3);
+  }
+  private calcTFIsub(array: {state: number;choice: number;}[], start: number, length: number): number {
+    let score: number = 0;
+    for(let i = start; i < start + length; i++) {
+      score += array[i].choice
+    }
+    score /= length;
+    score *= 10;
+    return score;
+  }
 
 }
