01e1389bbbb5fd954d2ccd3ab076c5ec8e79ef76
Liam Wynn <wynnliam@pdx.edu>
2019-04-11 15:32:37 -0700

    Added methods submitSurveyComponentTS, submitSurveyComponentTHS, and submitSurveyComponentTFI to survey.submit.ts

--- src/client_api/survey.submit.ts
+++ src/client_api/survey.submit.ts
@@ -1,5 +1,34 @@
+import { Utilities } from '../app/common/utlilities';
+
 export class SubmitSurveyHandler {
   public doSubmitSurvey() : void {
-    alert('We have submitted the survey!');
+    console.log('PATIENT INFORMATION');
+    console.log('----------------------------------------');
+    console.log(Utilities.getSessionStorage('patient-id'));
+    console.log('----------------------------------------');
+
+    console.log('SURVEY DATA');
+    console.log('----------------------------------------');
+
+    this.submitSurveyComponentTS();
+    this.submitSurveyComponentTHS();
+    this.submitSurveyComponentTFI();
+
+    console.log('----------------------------------------');
+  }
+
+  submitSurveyComponentTS() : void {
+    console.log('TS SURVEY RESULTS');
+    console.log(Utilities.getSessionStorage('ts-dataRecord'));
+  }
+
+  submitSurveyComponentTHS() : void {
+    console.log('THS SURVEY RESULTS');
+    console.log(Utilities.getSessionStorage('ths-dataRecord'));
+  }
+
+  submitSurveyComponentTFI() : void {
+    console.log('TFI SURVEY RESULTS');
+    console.log(Utilities.getSessionStorage('tfi-dataRecord'));
   }
 }
