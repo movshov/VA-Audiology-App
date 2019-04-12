62c5e27a3191902bbd46b18f67a8f578822d930a
Liam Wynn <wynnliam@pdx.edu>
2019-04-11 17:27:47 -0700

    Renamed survey.submit.test.ts to survey.submit.spec.ts

---
+++ src/client_api/survey.submit.spec.ts
@@ -0,0 +1,8 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SubmitSurveyHandler } from './survey.submit';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('Survey Submit Client API', () => {
+  console.log('Darth Shit Ass');
+});
--- src/client_api/survey.submit.test.ts
+++
@@ -1,5 +0,0 @@
-import { SubmitSurveyHandler } from './survey.submit';
-
-describe('Survey Submit Client API', () => {
-  console.log('Darth Shit Ass');
-});
