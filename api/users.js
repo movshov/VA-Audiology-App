4c109a9af89b32b57621d90c03411853356576b4
Elijah Rich-Wimmer <elij2@pdx.edu>
2019-04-15 23:41:36 -0700

    Removed unnecessary logging

--- api/users.js
+++ api/users.js
@@ -4,12 +4,11 @@ const db = require('./connectDB.js');
 
 //File structure analogous to an HTTP server: (URL looking for the file: 'localhost:3000/users')
 module.exports = handler((request, response) => {
-    const text = 'SELECT * FROM patient'
+    const text = 'SELECT * FROM patient';
     db.query(text, null, (err, res) => {
         if (err) {
             console.log(err.stack)
         } else {   
-            console.log("Hello1?");       
             response.status(200).json({
                 "status": "success",
                 "data": res.rows
