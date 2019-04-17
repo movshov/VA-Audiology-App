5755bc7807328c986b57cf9f1944e02c79c8f4c2
Liam Wynn <wynnliam@pdx.edu>
2019-04-17 12:53:07 -0700

    Commented out entire file src/API/app.ts for now

--- src/API/app.ts
+++ src/API/app.ts
@@ -4,27 +4,27 @@
 //
 
 // Imports
-import express = require('express');
-import bodyParser = require('body-parser');
+//import express = require('express');
+//import bodyParser = require('body-parser');
 
 // Globals
-const app = express() //Creates express app object
-const port = 3000
+//const app = express() //Creates express app object
+//const port = 3000
 
 
-app.use(bodyParser.json())
+/*app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
-)
+)*/
 
 // GET endpoint for the root
-app.get('/', (request, response) => {
+/*app.get('/', (request, response) => {
     response.json({ info: 'Postgress, Express, Angular, Node server reached. Greetings from Team E'})
-})
+})*/
 
 // Set app to listen on a given port
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log(`Starting VA App on port ${port}.`)
-})
+})*/
