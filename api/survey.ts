7aa4d731095a6bf8baf36215aa6da109b3eb3c89
Liriaene <DanteN.Ruth@gmail.com>
2019-04-17 16:03:13 -0700

    API-3: Changed getting data to this ugly garbage
    Having issuing getting the request data pulled in, so I switched to something that makes more sense to me for the time being (Granted it looks super awful). Needs to be chagned to something cleaner later

--- api/survey.ts
+++ api/survey.ts
@@ -12,21 +12,20 @@ const pool = new Pool({
 module.exports = handler((request : any) => {
 
   // Take data from request and build sql call from it
-  const {
-    tfisurveyid,
-    patientid,
-    completiondate,
-    completiontime,
-    tfi_i,
-    tfi_sc,
-    tfi_c,
-    tfi_si,
-    tfi_a,
-    tfi_r,
-    tfi_q,
-    tfi_e,
-    tfi_overallscore,
-  } = request
+  // TODO: Condenence this to something not ugly
+  var tfisurveyid = request.body.tfisurveyid
+  var patientid = request.body.patientid
+  var completiondate = request.body.completiondate
+  var completiontime = request.body.completiontime
+  var tfi_i = request.body.tfi_i
+  var tfi_sc = request.body.tfi_sc
+  var tfi_c = request.body.tfi_c
+  var tfi_si = request.body.tfi_si
+  var tfi_a = request.body.tfi_a
+  var tfi_r = request.body.tfi_r
+  var tfi_q = request.body.tfi_q
+  var tfi_e = request.body.tfi_e
+  var tfi_overallscore = request.body.tfi_overallscore
   
   var tfisurvey_sql = "INSERT INTO tfisurvey (tfisurveyid, patientid, completiondate, completiontime, tfi_i, tfi_sc, tfi_c, tfi_si, tfi_a, tfi_r, tfi_q, tfi_e, tfi_overallscore) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)"
 
