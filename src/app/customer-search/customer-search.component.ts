85af2f15ee6d35565888dda5a3090660d9bc9341
Dominic Okapal <djo@pdx.edu>
2019-04-18 14:49:22 -0700

    Created customer-search.compnent

---
+++ src/app/customer-search/customer-search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-search',
+  templateUrl: './customer-search.component.html',
+  styleUrls: ['./customer-search.component.css']
+})
+export class CustomerSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public patientSearch(event) {
+    console.log('event: ' + event);
+  }
+
+}
