d657ef2ef36051680bc641eb835d2194a82eb4fa
Dominic Okapal <djo@pdx.edu>
2019-04-22 14:50:50 -0700

    Added password strength checking, and checks that new passwords match

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'my-account',
@@ -6,25 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
-  public oldPass;
-  public newPass;
-  public verifyPass;
+  public oldPass: number = 0;
+  public newPass: number = 0;
+  public verifyPass: number = 0;
+  public newPassField: string = '';
+  public verifyPassField: string = '';
 
   constructor() { }
 
   public ngOnInit() {
   }
 
-  public prevPassIsValid(): boolean {
-    return true;
+  public oldPassChange(event: string) {
+    if(this.oldPassIsValid(event)) {this.oldPass = 1;}
+    else if(event === '') {this.oldPass = 0;}
+    else {this.oldPass = -1;}
+  }
+  public newPassChange(event: string) {
+    this.newPass = this.passwordStrength(event);
+    this.verifyPassChange(this.verifyPassField);
+  }
+  public verifyPassChange(event: string) {
+    if(event === this.newPassField) {this.verifyPass = 1;}
+    else if(event === '') {this.verifyPass = 0;}
+    else {this.verifyPass = -1;}
   }
 
-  public newPassIsValid(): boolean {
-    return false;
+  // CHANGE this to check with actual password
+  private oldPassIsValid(password: string): boolean {
+    return password === 'password';
   }
+  private passwordStrength(password: string): number {
+    if(password === '') { return 0;}
+    let total = 0;
+    total += password.toUpperCase() !== password ? 1 : 0; // has lower
+    total += password.toLowerCase() !== password ? 1 : 0; // has upper
+    total += /(!|@|#|\$|%|\^|\&|\*|\(|\)|-|\+)/.test(password) ? 1 : 0; // has special
+    total += /\d/.test(password) ? 1 : 0; // has digit
+    total += password.length >= 8 ? 1 : 0; // length at least 8
 
-  public verifyPassIsValid(): boolean {
-    return this.newPass === this.verifyPass;
+    if(total < 3) { return -1;}
+    if(total < 5) { return 1;}
+    return 2;
   }
 
 }
