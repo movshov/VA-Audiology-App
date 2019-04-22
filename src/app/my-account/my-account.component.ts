50439a07b6359a7ce083fcca60d0f20517cf5b52
Dominic Okapal <djo@pdx.edu>
2019-04-22 12:56:29 -0700

    added fields and css

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-my-account',
+  selector: 'my-account',
   templateUrl: './my-account.component.html',
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
+  public oldPass;
+  public newPass;
+  public verifyPass;
 
   constructor() { }
 
   public ngOnInit() {
+  }
+
+  public prevPassIsValid(): boolean {
+    return true;
+  }
+
+  public newPassIsValid(): boolean {
+    return false;
+  }
+
+  public verifyPassIsValid(): boolean {
+    return this.newPass === this.verifyPass;
   }
 
 }
