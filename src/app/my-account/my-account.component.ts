b25c7b0a056d79461511a9c2374a37a54cb86b3a
Dominic Okapal <djo@pdx.edu>
2019-04-23 16:10:31 -0700

    Merged variables, added messages

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -6,44 +6,72 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
-  public oldPass: number = 0;
-  public newPass: number = 0;
-  public verifyPass: number = 0;
-  public newPassField: string = '';
-  public verifyPassField: string = '';
+  public messages: Object = {warning: false, display: false, message: ''};
+  public passColors: Object = {oldPass: 0, newPass: 0, verifyPass: 0};
+  public passFields: Object = {oldPassField: '', newPassField: '', verifyPassField: ''};
+
+  //TODO: !!!!! REMOVE THIS WHEN CONNECTS TO DB!!!!!//
+  private pword: string = 'Passw0rd!';
+  /////////////////////////////////////////////
 
   constructor() { }
 
   public ngOnInit() {
   }
 
-  public oldPassChange(event: string) {
-    if(this.oldPassIsValid(event)) {this.oldPass = 1;}
-    else if(event === '') {this.oldPass = 0;}
-    else {this.oldPass = -1;}
+  public oldPassChange() {
+    if(this.oldPassIsValid()) {this.passColors['oldPass'] = 1;}
+    else if(this.passFields['oldPassField'] === '') {this.passColors['oldPass'] = 0;}
+    else {this.passColors['oldPass'] = -1;}
   }
-  public newPassChange(event: string) {
-    this.newPass = this.passwordStrength(event);
-    this.verifyPassChange(this.verifyPassField);
+  public newPassChange() {
+    this.passColors['newPass'] = this.passwordStrength();
+    this.verifyPassChange();
   }
-  public verifyPassChange(event: string) {
-    if(event === this.newPassField) {this.verifyPass = 1;}
-    else if(event === '') {this.verifyPass = 0;}
-    else {this.verifyPass = -1;}
+  public verifyPassChange() {
+    if(this.passFields['verifyPassField'] === '' && this.passFields['newPassField'] === '') {this.passColors['verifyPass'] = 0;}
+    else if(this.passFields['verifyPassField'] === this.passFields['newPassField']) {this.passColors['verifyPass'] = 1;}
+    else {this.passColors['verifyPass'] = -1;}
+  }
+  public onSubmit() {
+    this.messages['display'] = false;
+    if(!this.oldPassIsValid()) {
+      this.messages['message'] = 'Old Password Is Invalid!';
+      this.messages['warning'] = true;
+    } else if(this.passFields['verifyPassField'] !== this.passFields['newPassField']) {
+      this.messages['message'] = 'New Passwords Do Not Match!';
+      this.messages['warning'] = true;
+    } else if(this.passFields['newPassField'] === '') {
+      this.messages['message'] = 'You Must Enter A New Password!';
+      this.messages['warning'] = true;
+    } else if(this.passColors['newPass'] < 1) {
+      this.messages['message'] = 'Choose A Stronger Password!';
+      this.messages['warning'] = true;
+    } else if(this.passFields['oldPassField'] === this.passFields['newPassField']) {
+      this.messages['message'] = 'Choose A New Password!';
+      this.messages['warning'] = true;
+    } else {
+      // TODO: change this to talk to db
+      this.pword = this.passFields['newPassField'];
+      this.messages['message'] = 'Password Changed';
+      this.messages['warning'] = false;
+    }
+    this.messages['display'] = true;
+    console.log(this.pword);
   }
 
-  // CHANGE this to check with actual password
-  private oldPassIsValid(password: string): boolean {
-    return password === 'password';
+  // TODO: CHANGE this to check with actual password
+  private oldPassIsValid(): boolean {
+    return this.passFields['oldPassField'] === this.pword;
   }
-  private passwordStrength(password: string): number {
-    if(password === '') { return 0;}
+  private passwordStrength(): number {
+    if(this.passFields['newPassField'] === '') { return 0;}
     let total = 0;
-    total += password.toUpperCase() !== password ? 1 : 0; // has lower
-    total += password.toLowerCase() !== password ? 1 : 0; // has upper
-    total += /(!|@|#|\$|%|\^|\&|\*|\(|\)|-|\+)/.test(password) ? 1 : 0; // has special
-    total += /\d/.test(password) ? 1 : 0; // has digit
-    total += password.length >= 8 ? 1 : 0; // length at least 8
+    total += this.passFields['newPassField'].toUpperCase() !== this.passFields['newPassField'] ? 1 : 0; // has lower
+    total += this.passFields['newPassField'].toLowerCase() !== this.passFields['newPassField'] ? 1 : 0; // has upper
+    total += /(!|@|#|\$|%|\^|\&|\*|\(|\)|-|\+)/.test(this.passFields['newPassField']) ? 1 : 0; // has special
+    total += /\d/.test(this.passFields['newPassField']) ? 1 : 0; // has digit
+    total += this.passFields['newPassField'].length >= 8 ? 1 : 0; // length at least 8
 
     if(total < 3) { return -1;}
     if(total < 5) { return 1;}
