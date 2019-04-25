a380691daf4c9f5a4db11b113b8888b75d09e3ef
Dominic Okapal <djo@pdx.edu>
2019-04-25 14:12:01 -0700

    Created color object to make code more readable, made improvements to css

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -6,76 +6,100 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
-  public messages: Object = {warning: false, display: false, message: ''};
-  public passColors: Object = {oldPass: 0, newPass: 0, verifyPass: 0};
-  public passFields: Object = {oldPassField: '', newPassField: '', verifyPassField: ''};
+  public colors: { red: number, green: number, yellow: number, blue: number, red2: number, green2: number };
+  public messages: { color: number, display: boolean, message: string };
+  public passColors: { oldPass: number, newPass: number, verifyPass: number };
+  public passFields: { oldPassField: string, newPassField: string, verifyPassField: string };
+  public submitDisabled: boolean = false;
+
 
   //TODO: !!!!! REMOVE THIS WHEN CONNECTS TO DB!!!!!//
   private pword: string = 'Passw0rd!';
   /////////////////////////////////////////////
 
-  constructor() { }
+  constructor() {
+    this.colors = { red: 0, green: 1, yellow: 2, blue: 3, red2: 4, green2: 5 };
+    this.messages = { color: this.colors.red, display: false, message: '' };
+    this.passColors = { oldPass: this.colors.blue, newPass: this.colors.blue, verifyPass: this.colors.blue };
+    this.passFields = { oldPassField: '', newPassField: '', verifyPassField: '' };
+   }
 
   public ngOnInit() {
   }
 
-  public oldPassChange() {
-    if(this.oldPassIsValid()) {this.passColors['oldPass'] = 1;}
-    else if(this.passFields['oldPassField'] === '') {this.passColors['oldPass'] = 0;}
-    else {this.passColors['oldPass'] = -1;}
+  public oldPassChange(): void {
+    this.passColors.oldPass = this.colors.blue;
   }
-  public newPassChange() {
-    this.passColors['newPass'] = this.passwordStrength();
+  public newPassChange(): void {
+    this.passColors.newPass = this.passwordStrength();
     this.verifyPassChange();
   }
-  public verifyPassChange() {
-    if(this.passFields['verifyPassField'] === '' && this.passFields['newPassField'] === '') {this.passColors['verifyPass'] = 0;}
-    else if(this.passFields['verifyPassField'] === this.passFields['newPassField']) {this.passColors['verifyPass'] = 1;}
-    else {this.passColors['verifyPass'] = -1;}
+  public verifyPassChange(): void {
+    if (this.passFields.verifyPassField === '' && this.passFields.newPassField === '') { this.passColors.verifyPass = this.colors.blue; }
+    else if (this.passFields.verifyPassField === this.passFields.newPassField) { this.passColors.verifyPass = this.colors.green; }
+    else { this.passColors.verifyPass = this.colors.red; }
   }
-  public onSubmit() {
-    this.messages['display'] = false;
-    if(!this.oldPassIsValid()) {
-      this.messages['message'] = 'Old Password Is Invalid!';
-      this.messages['warning'] = true;
-    } else if(this.passFields['verifyPassField'] !== this.passFields['newPassField']) {
-      this.messages['message'] = 'New Passwords Do Not Match!';
-      this.messages['warning'] = true;
-    } else if(this.passFields['newPassField'] === '') {
-      this.messages['message'] = 'You Must Enter A New Password!';
-      this.messages['warning'] = true;
-    } else if(this.passColors['newPass'] < 1) {
-      this.messages['message'] = 'Choose A Stronger Password!';
-      this.messages['warning'] = true;
-    } else if(this.passFields['oldPassField'] === this.passFields['newPassField']) {
-      this.messages['message'] = 'Choose A New Password!';
-      this.messages['warning'] = true;
+  public onSubmit(): void {
+    this.submitDisabled = true;
+    this.messages.display = true;
+    let warn: boolean = true;
+    if (this.passFields.oldPassField === '') {
+      this.passColors.oldPass = this.colors.red;
+      this.messages.message = 'You Must Enter Your Old Password!';
+    } else if (this.passFields.newPassField === '') {
+      this.messages.message = 'You Must Enter A New Password!';
+    } else if (this.passColors.newPass === this.colors.red) {
+      this.messages.message = 'Choose A Stronger Password!';
+    } else if (this.passFields.verifyPassField !== this.passFields.newPassField) {
+      this.messages.message = 'New Passwords Do Not Match!';
+    } else if (this.passFields.oldPassField === this.passFields.newPassField) {
+      this.messages.message = 'Choose A New Password!';
+    } else if (!this.oldPassChanged()) {
+      this.passColors.oldPass = this.colors.red;
+      this.messages.message = 'Old Password Is Invalid!';
     } else {
-      // TODO: change this to talk to db
-      this.pword = this.passFields['newPassField'];
-      this.messages['message'] = 'Password Changed';
-      this.messages['warning'] = false;
+      // TODO: REMOVE the following line when we connect to DB
+      this.pword = this.passFields.newPassField;
+      this.messages.message = 'Password Changed';
+      warn = false;
+      this.passColors.oldPass = this.colors.green;
     }
-    this.messages['display'] = true;
+    this.animWarn(warn);
+    this.submitDisabled = false;
     console.log(this.pword);
   }
 
-  // TODO: CHANGE this to check with actual password
-  private oldPassIsValid(): boolean {
-    return this.passFields['oldPassField'] === this.pword;
+  // TODO: CHANGE this to check with actual password in DB
+  private oldPassChanged(): boolean {
+    return this.passFields.oldPassField === this.pword;
   }
   private passwordStrength(): number {
-    if(this.passFields['newPassField'] === '') { return 0;}
-    let total = 0;
-    total += this.passFields['newPassField'].toUpperCase() !== this.passFields['newPassField'] ? 1 : 0; // has lower
-    total += this.passFields['newPassField'].toLowerCase() !== this.passFields['newPassField'] ? 1 : 0; // has upper
-    total += /(!|@|#|\$|%|\^|\&|\*|\(|\)|-|\+)/.test(this.passFields['newPassField']) ? 1 : 0; // has special
-    total += /\d/.test(this.passFields['newPassField']) ? 1 : 0; // has digit
-    total += this.passFields['newPassField'].length >= 8 ? 1 : 0; // length at least 8
+    if (this.passFields.newPassField === '') { return this.colors.blue; }
+    let total: number = 0;
+    total += this.passFields.newPassField.toUpperCase() !== this.passFields.newPassField ? 1 : 0; // has lower
+    total += this.passFields.newPassField.toLowerCase() !== this.passFields.newPassField ? 1 : 0; // has upper
+    total += /(!|@|#|\$|%|\^|\&|\*|\(|\)|-|\+)/.test(this.passFields.newPassField) ? 1 : 0; // has special
+    total += /\d/.test(this.passFields.newPassField) ? 1 : 0; // has digit
+    total += this.passFields.newPassField.length >= 8 ? 1 : 0; // length at least 8
 
-    if(total < 3) { return -1;}
-    if(total < 5) { return 1;}
-    return 2;
+    if (total < 3) { return this.colors.red; }
+    if (total < 5) { return this.colors.yellow; }
+    return this.colors.green;
+  }
+  private animWarn(warn: boolean): void {
+    if (this.messages.color === this.colors.red || this.messages.color === this.colors.red2) {
+      if (warn) {
+        this.messages.color = this.messages.color === this.colors.red ? this.colors.red2 : this.colors.red;
+      } else {
+        this.messages.color = this.colors.green;
+      }
+    } else {
+      if (warn) {
+        this.messages.color = this.colors.red;
+      } else {
+        this.messages.color = this.messages.color === this.colors.green ? this.colors.green2 : this.colors.green;
+      }
+    }
   }
 
 }
