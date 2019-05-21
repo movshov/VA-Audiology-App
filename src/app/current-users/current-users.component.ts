2a3672ee7da41be79d0a513c7d7b01d61e938754
Elijah Rich-Wimmer <elij2@pdx.edu>
2019-05-21 03:16:10 -0700

    Connected Users Table to service

--- src/app/current-users/current-users.component.ts
+++ src/app/current-users/current-users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Users } from './users';
 import { UsersObject } from '../../../api-objects/UsersObject';
+import { ApiUsersCrudService } from '../services/api-users-crud.service';
+import { NotificationService } from '../services/notification.service';
 
 @Component({
   selector: 'current-users',
@@ -13,63 +15,75 @@ export class CurrentUsersComponent implements OnInit {
   public usersTable: UsersObject[] = [];
   public pageCounter: number = 0;
 
-  constructor(/*private currentusersTableService: GetCurrentUsersService*/) { }
+  constructor(private apiUsersCrudService: ApiUsersCrudService, private notificationService: NotificationService) { }
 
   public ngOnInit() {
     this.getAllUsers();
-}
+  }
 
- public getAllUsers(): void {
-  //  this.currentUsersService.getUsers()
-  //  .subscribe(
-  //    (results) => {
-  //      this.usersTable = results.data;
-  //      console.log(this.usersTable);
-  //      console.log('Users loading...');
+  public getAllUsers(): void {
+    this.apiUsersCrudService.getUsers()
+      .subscribe(
+        (results) => {
+          this.usersTable = results.data;
+        });
+    this.usersTable = Users;
+    this.usersTable.map((types) => {
+      if (types.authorityType === 0) { } else { }
+    });
+  }
 
-  //  },(error) => {
-  //    console.log('There was an error' + error);
-  //  });
+  public prevPage(pageNum: number): void {
+    while (this.pageCounter > 0 && pageNum > 0) {
+      this.pageCounter -= 1;
+      pageNum -= 1;
+    }
+  }
 
-  this.usersTable = Users;
-  // this.usersTable.map((types) => {
-  //   if(types.authorityType === 0) {} else {}
-  // });
- }
+  public nextPage(pageNum: number): void {
+    while ((this.pageCounter + 1) * 10 < this.usersTable.length && pageNum > 0) {
+      this.pageCounter += 1;
+      pageNum += 1;
+    }
+  }
 
- public prevPage(pageNum: number): void {
-   while( this.pageCounter > 0 && pageNum > 0) {
-     this.pageCounter -= 1;
-     pageNum -= 1;
-   }
- }
+  /**
+   * Deletes a user at a given index.
+   * @param user a user object to be deleted
+   */
+  public deleteUser(user: UsersObject) {
+    this.apiUsersCrudService.deleteUser(user.username).subscribe(
+      (_) => {
+        let index: number = this.usersTable.indexOf(user);
+        this.usersTable.splice(index, 1);
+        this.notificationService.showSuccess('User '+ user.username + ' was successfully deleted');
+      }
+    )
+  }
 
- public nextPage(pageNum: number): void {
-   while((this.pageCounter + 1) * 10 < this.usersTable.length && pageNum > 0) {
-     this.pageCounter += 1;
-     pageNum += 1;
-   }
- }
+  /**
+   * Updates the username of the user, at a given index.
+   * @param f NgForm that contains the value inside the form tag in the template view
+   * @param update UsersObject that keeps track of the index.
+   */
+  public updateUser(f: NgForm, update: UsersObject) {
+    let index: number = this.usersTable.indexOf(update);
+    if (f.value.username !== '') {
+      this.apiUsersCrudService.updateUsername(update.username, f.value).subscribe(
+        (_) => {
+          this.notificationService.showSuccess('Username, ' + update.username + ' was successfully updated to ' + f.value);
+          this.usersTable[index].username = f.value.username;
+        }
+      )
+    }
+  }
 
- /**
-  * Deletes a user at a given index.
-  * @param user a user object to be deleted
-  */
- public deleteUser(user: UsersObject) {
-   let index: number = this.usersTable.indexOf(user);
-   this.usersTable.splice(index, 1);
- }
-
- /**
-  * Updates the username of the user, at a given index.
-  * @param f NgForm that contains the value inside the form tag in the template view
-  * @param update UsersObject that keeps track of the index.
-  */
- public updateUser(f: NgForm , update: UsersObject) {
-   let index: number = this.usersTable.indexOf(update);
-   if(f.value.username !== '') {
-     this.usersTable[index].username = f.value.username;
-   }
- }
+  public resetPassword(user: UsersObject) {
+    this.apiUsersCrudService.resetPassword(user.username).subscribe(
+      result => {
+        this.notificationService.showSuccess('NEW PASSWORD FOR ' + user.username + ' IS: ' + result.data);
+      }
+    )
+  }
 
 }
