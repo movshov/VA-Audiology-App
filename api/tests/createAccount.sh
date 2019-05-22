89876a17bedbab6c097578665085a61ed8fc5f2b
Griffin Stone <gstone@pdx.edu>
2019-05-22 00:14:44 -0700

    Update test

--- api/tests/createAccount.sh
+++ api/tests/createAccount.sh
@@ -5,12 +5,12 @@ source login.sh
 name="test$RANDOM"
 
 echo -e '\nInsufficient permissions'
-curlHeaders localhost:3333/accounts/create -d "username=$name&authorityName=TestAuthority&authorityType=1"
+curlHeaders localhost:3333/accounts/create -d '{"user":{"username":"'"$name"'", "name":"TestAuthority", "authorityType":1}, "adminPassword":"password1"}' -H 'Content-Type: application/json'
 
 login Stat qwertyuiop
 
 echo -e '\nShould Succeed'
-result="$(curlHeaders localhost:3333/accounts/create -d "username=$name&authorityName=TestAuthority&authorityType=1")"
+result="$(curlHeaders localhost:3333/accounts/create -d '{"user":{"username":"'"$name"'", "name":"TestAuthority", "authorityType":1}, "adminPassword":"qwertyuiop"}' -H 'Content-Type: application/json')"
 echo $result
 password=`echo $result | extractField password`
 echo $password
@@ -19,8 +19,8 @@ echo -e '\nShould Login'
 login $name $password
 
 echo -e '\nDuplicate Insertion'
-curlHeaders localhost:3333/accounts/create -d "username=$name&authorityName=TestShouldFail&authorityType=0"
+curlHeaders localhost:3333/accounts/create -d '{"user":{"username":"'"$name"'", "name":"TestShouldFail", "authorityType":0}, "adminPassword":"'"$password"'"}' -H 'Content-Type: application/json'
 
 echo -e '\nMissing Argument'
-curlHeaders localhost:3333/accounts/create -d "authorityName=TestShouldFail&authorityType=0"
+curlHeaders localhost:3333/accounts/create -d '{"user":{"username":"'"$name"'", "authorityType":0}, "adminPassword":"'"$password"'"}' -H 'Content-Type: application/json'
 
