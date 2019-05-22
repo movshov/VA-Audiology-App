aa21e1170eec48c1adf1bb607003664c88c2f56e
Elijah Rich-Wimmer <elij2@pdx.edu>
2019-05-21 22:53:39 -0700

    Added Email Support

--- api-objects/UsersObject.ts
+++ api-objects/UsersObject.ts
@@ -8,15 +8,12 @@ export const authorityTypes: Array<string> = ['Audiologist', 'Admin'];
 export class UsersObject {
     constructor(
         public username: string,
-        public name: string,
-        public email: string,
+        public authorityid: string,
+        public authorityemail: string,
         public authorityType: number,
     ) {}
-	public authorityid: number;
 	public authorityname: string;
-	public authorityemail: string;
 	public password: string;
-	public authoritytype: number;
 }
 
 export class CreateUserRequest {
