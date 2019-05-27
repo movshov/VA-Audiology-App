5876a5572cb2c54e1742be51f231aa8a87cf7b09
Elijah Rich-Wimmer <elij2@pdx.edu>
2019-05-27 13:27:10 -0700

    Added Admin Password Confirmation Popup

---
+++ client/src/app/admin-password-confirm/admin-password-confirm.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+export interface ReloginData {
+  password: string;
+}
+
+@Component({
+  selector: 'admin-password-confirm',
+  templateUrl: './admin-password-confirm.html',
+  styleUrls: ['./admin-password-confirm.css']
+})
+export class AdminPasswordConfirm implements OnInit {
+
+  public data: ReloginData = {password: ''};
+  
+  constructor(
+    private diaglogRef: MatDialogRef<AdminPasswordConfirm>,
+    ) {}
+
+  ngOnInit() {
+  }
+
+  private onSubmit(): void {
+    this.diaglogRef.close(this.data.password);
+  }
+}
